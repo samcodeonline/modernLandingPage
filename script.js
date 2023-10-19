68c32e1201188f4972d115f8c56434af28ef56d8
sarim nadeem <115877996+samcodeonline@users.noreply.github.com>
2023-10-19 18:02:41 +0500

    change

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
+// var cloudTl = new TimelineMax({repeat:-1});
+gsap.to(".imageContainer",{
+    ease:Expo.easeInOut,
+    width: "100%",
+    stagger:2
+    
+})
 
-    gsap.to(".imageContainer",{
-        ease:Expo.easeInOut,
-        width: "100%",
-        stagger:2
-        
-    })
+gsap.to(".text h1",{
+    ease:Expo.easeInOut,
+    // duration:2,
+    stagger:2,
+    top: 0
+})
 
-    gsap.to(".text h1",{
-        ease:Expo.easeInOut,
-        // duration:2,
-        stagger:2,
-        top: 0
-    })
-
-    gsap.to(".text h1",{
-        delay :2,
-        ease:Expo.easeInOut,
-        stagger:2,
-        top: "-100%"
-    })
+gsap.to(".text h1",{
+    delay :2,
+    ease:Expo.easeInOut,
+    stagger:2,
+    top: "-100%"
+})
